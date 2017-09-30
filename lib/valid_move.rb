5190d5e7379b0853e87440a93a22756778a48489
Matthew Hunger <matt.hunger@gmail.com>
2017-09-30 10:50:30 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,14 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  
+  if !(position_taken?(board, index))
+    if index.between(0-8)
+      return true
+    else
+      return false
+    else 
+      return false
+    end
+  end
 end
 
 
